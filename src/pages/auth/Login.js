77340d9970105e64573325d8b01f9c621c9b53e3
chris097 <christian@esettlementgroup.com>
2022-08-25 07:24:37 +0100

    updated login page

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { useFormik } from 'formik';
 import { Link } from 'react-router-dom';
 import Input from '../../components/Input';
 import Logo from '../../public/svgs/Logo';
 import { PRIVATE_ROUTE } from '../../routes/url';
+import { loginSchema } from '../../validator';
 
 const Login = () => {
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    validationSchema: loginSchema,
+    onSubmit: values => {
+      console.log(values)
+    }
+  });
+
   return (
     <div className='mx-auto flex justify-center'>
       <div className='w-365px h-auto shadow-baseshadow bg-white px-6 py-8 mt-20 rounded-md'>
@@ -13,22 +27,32 @@ const Login = () => {
           <div className='text-xl text-primaryblue'>Hello</div>
           <div className='text-xl text-primaryblue'>welcome back</div>
           <div className='text-sm text-textgray my-3'>Enter your personal details to login in</div>
-          <form>
+          <form onSubmit={formik.handleSubmit}>
             <Input
               label='Email'
               input={{
                 type: 'email',
                 placeholder: 'Email',
+                id: 'email',
+                name: 'email',
+                ...formik.getFieldProps('email')
               }}
             />
+            {formik.touched.email && formik.errors.email ? (<span className='text-xs text-red-500'>email is required.</span>): null}
             <Input
               label='Password'
               input={{
                 type: 'password',
                 placeholder: 'Password',
+                name: 'password',
+                id: 'password',
+                ...formik.getFieldProps('password')
               }}
             />
-            <button className='bg-primaryblue text-white w-full h-12 rounded-full mt-5' type='submit'>Login</button>
+            {formik.touched.password && formik.errors.password ? (<span className='text-xs text-red-500'>firstname is required.</span>):null}
+            <button className='bg-primaryblue text-white w-full h-12 rounded-full mt-5' type='submit'>
+              {formik.isSubmitting ? 'Loading...' : 'Login'}
+            </button>
           </form>
           <Link to={PRIVATE_ROUTE.AUTH_FORGOT_PASSWORD}>
             <div className='flex justify-end text-sm text-primaryblue mt-2'>Forgot Password?</div>
