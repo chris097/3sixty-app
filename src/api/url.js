db4c17196a0ce0ded1e2a3d7067e00feadf50826
chris097 <christian@esettlementgroup.com>
2022-09-06 13:50:55 +0100

    reset password link

--- src/api/url.js
+++ src/api/url.js
@@ -1,6 +1,8 @@
 export const apiUrl = {
+    POST: 'POST',
     REGISTER_URL: 'user/register',
     LOGIN_URL: 'user/login',
+    RESET_PASSWORD_URL: 'user/reset-password',
     DASHBOARD_NOTE_URL: 'notes',
     EDIT_DASHBOARD_NOTE_URL: (id) => `note/${id}`,
     DELETE_DASHBOARD_NOTE_URL: (id) => `note/${id}`,
--- src/routes/url.js
+++ src/routes/url.js
@@ -2,6 +2,7 @@ export const PRIVATE_ROUTE = {
     AUTH_NOT_FOUND: '*',
     AUTH_LOGIN: '/auth/login',
     AUTH_REGISTER: '/auth/register',
+    AUTH_MAIL_SENT: '/auth/mail-sent',
     AUTH_FORGOT_PASSWORD: '/auth/forgot-password',
     AUTH_RESET_PASSWORD: '/auth/reset-password/:token'
 };
