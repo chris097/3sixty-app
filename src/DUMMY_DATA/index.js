bbf5d0459d10fccdab4242d4bb17dba3866d42b2
chris097 <christian@esettlementgroup.com>
2022-09-21 10:06:01 +0100

    update

--- src/DUMMY_DATA/index.js
+++ src/DUMMY_DATA/index.js
@@ -23,47 +23,47 @@ export const DROP_DOWNS = [
   { id: 4, icon: <Edit />, name: 'Archive' },
 ]
 
-export const DART_CARDS = [
-  {
-    id: 1,
-    tag: 'personal',
-    title: '10 Hours Fasting every week',
-    description: 'Exercitation in consectetur velit enim ullamco aliqua officia mollit eu nulla sunt aliquip aute nulla. Aliquip tempor et exercitation minim adipisicing minim velit. Id elit culpa aute est. Aute aute excepteur et adipisicing adipisicing officia dolor. Commodo incididunt ad labore incididunt aliquip deserunt anim eu incididunt est mollit. Proident aliqua non aliquip dolore sint voluptate adipisicing. Tempor in fugiat incididunt ea officia ullamco ut qui excepteur dolore ut et ut. Ut do laborum culpa consectetur duis et velit est nulla. In consectetur nisi duis ad quis magna anim ut dolor minim irure proident do irure. Sit do quis sunt consequat id incididunt quis.',
-    created_at: '23 hours ago'
-  },
-  {
-    id: 2,
-    tag: 'personal',
-    title: 'Birthday debrief',
-    description: 'Exercitation in consectetur velit enim ullamco aliqua officia mollit eu nulla sunt aliquip aute nulla. Aliquip tempor et exercitation minim adipisicing minim velit. Id elit culpa aute est. Aute aute excepteur et adipisicing adipisicing officia dolor. Commodo incididunt ad labore incididunt aliquip deserunt anim eu incididunt est mollit. Proident aliqua non aliquip dolore sint voluptate adipisicing. Tempor in fugiat incididunt ea officia ullamco ut qui excepteur dolore ut et ut. Ut do laborum culpa consectetur duis et velit est nulla. In consectetur nisi duis ad quis magna anim ut dolor minim irure proident do irure. Sit do quis sunt consequat id incididunt quis.',
-    created_at: '23 hours ago'
-  },
-  {
-    id: 3,
-    tag: 'personal',
-    title: 'Visit Plateau State',
-    description: 'Exercitation in consectetur velit enim ullamco aliqua officia mollit eu nulla sunt aliquip aute nulla. Aliquip tempor et exercitation minim adipisicing minim velit. Id elit culpa aute est. Aute aute excepteur et adipisicing adipisicing officia dolor. Commodo incididunt ad labore incididunt aliquip deserunt anim eu incididunt est mollit. Proident aliqua non aliquip dolore sint voluptate adipisicing. Tempor in fugiat incididunt ea officia ullamco ut qui excepteur dolore ut et ut. Ut do laborum culpa consectetur duis et velit est nulla. In consectetur nisi duis ad quis magna anim ut dolor minim irure proident do irure. Sit do quis sunt consequat id incididunt quis.',
-    created_at: '23 hours ago'
-  },
-  {
-    id: 4,
-    tag: 'personal',
-    title: 'Learn Dart',
-    description: 'Exercitation in consectetur velit enim ullamco aliqua officia mollit eu nulla sunt aliquip aute nulla. Aliquip tempor et exercitation minim adipisicing minim velit. Id elit culpa aute est. Aute aute excepteur et adipisicing adipisicing officia dolor. Commodo incididunt ad labore incididunt aliquip deserunt anim eu incididunt est mollit. Proident aliqua non aliquip dolore sint voluptate adipisicing. Tempor in fugiat incididunt ea officia ullamco ut qui excepteur dolore ut et ut. Ut do laborum culpa consectetur duis et velit est nulla. In consectetur nisi duis ad quis magna anim ut dolor minim irure proident do irure. Sit do quis sunt consequat id incididunt quis.',
-    created_at: '23 hours ago'
-  },
-  {
-    id: 5,
-    tag: 'personal',
-    title: 'Learn Dart',
-    description: 'Exercitation in consectetur velit enim ullamco aliqua officia mollit eu nulla sunt aliquip aute nulla. Aliquip tempor et exercitation minim adipisicing minim velit. Id elit culpa aute est. Aute aute excepteur et adipisicing adipisicing officia dolor. Commodo incididunt ad labore incididunt aliquip deserunt anim eu incididunt est mollit. Proident aliqua non aliquip dolore sint voluptate adipisicing. Tempor in fugiat incididunt ea officia ullamco ut qui excepteur dolore ut et ut. Ut do laborum culpa consectetur duis et velit est nulla. In consectetur nisi duis ad quis magna anim ut dolor minim irure proident do irure. Sit do quis sunt consequat id incididunt quis.',
-    created_at: '23 hours ago'
-  },
-  {
-    id: 6,
-    tag: 'personal',
-    title: 'Learn Dart',
-    description: 'Exercitation in consectetur velit enim ullamco aliqua officia mollit eu nulla sunt aliquip aute nulla. Aliquip tempor et exercitation minim adipisicing minim velit. Id elit culpa aute est. Aute aute excepteur et adipisicing adipisicing officia dolor. Commodo incididunt ad labore incididunt aliquip deserunt anim eu incididunt est mollit. Proident aliqua non aliquip dolore sint voluptate adipisicing. Tempor in fugiat incididunt ea officia ullamco ut qui excepteur dolore ut et ut. Ut do laborum culpa consectetur duis et velit est nulla. In consectetur nisi duis ad quis magna anim ut dolor minim irure proident do irure. Sit do quis sunt consequat id incididunt quis.',
-    created_at: '23 hours ago'
-  },
-];
+// export const DART_CARDS = [
+//   {
+//     id: 1,
+//     tag: 'personal',
+//     title: '10 Hours Fasting every week',
+//     description: 'Exercitation in consectetur velit enim ullamco aliqua officia mollit eu nulla sunt aliquip aute nulla. Aliquip tempor et exercitation minim adipisicing minim velit. Id elit culpa aute est. Aute aute excepteur et adipisicing adipisicing officia dolor. Commodo incididunt ad labore incididunt aliquip deserunt anim eu incididunt est mollit. Proident aliqua non aliquip dolore sint voluptate adipisicing. Tempor in fugiat incididunt ea officia ullamco ut qui excepteur dolore ut et ut. Ut do laborum culpa consectetur duis et velit est nulla. In consectetur nisi duis ad quis magna anim ut dolor minim irure proident do irure. Sit do quis sunt consequat id incididunt quis.',
+//     created_at: '23 hours ago'
+//   },
+//   {
+//     id: 2,
+//     tag: 'personal',
+//     title: 'Birthday debrief',
+//     description: 'Exercitation in consectetur velit enim ullamco aliqua officia mollit eu nulla sunt aliquip aute nulla. Aliquip tempor et exercitation minim adipisicing minim velit. Id elit culpa aute est. Aute aute excepteur et adipisicing adipisicing officia dolor. Commodo incididunt ad labore incididunt aliquip deserunt anim eu incididunt est mollit. Proident aliqua non aliquip dolore sint voluptate adipisicing. Tempor in fugiat incididunt ea officia ullamco ut qui excepteur dolore ut et ut. Ut do laborum culpa consectetur duis et velit est nulla. In consectetur nisi duis ad quis magna anim ut dolor minim irure proident do irure. Sit do quis sunt consequat id incididunt quis.',
+//     created_at: '23 hours ago'
+//   },
+//   {
+//     id: 3,
+//     tag: 'personal',
+//     title: 'Visit Plateau State',
+//     description: 'Exercitation in consectetur velit enim ullamco aliqua officia mollit eu nulla sunt aliquip aute nulla. Aliquip tempor et exercitation minim adipisicing minim velit. Id elit culpa aute est. Aute aute excepteur et adipisicing adipisicing officia dolor. Commodo incididunt ad labore incididunt aliquip deserunt anim eu incididunt est mollit. Proident aliqua non aliquip dolore sint voluptate adipisicing. Tempor in fugiat incididunt ea officia ullamco ut qui excepteur dolore ut et ut. Ut do laborum culpa consectetur duis et velit est nulla. In consectetur nisi duis ad quis magna anim ut dolor minim irure proident do irure. Sit do quis sunt consequat id incididunt quis.',
+//     created_at: '23 hours ago'
+//   },
+//   {
+//     id: 4,
+//     tag: 'personal',
+//     title: 'Learn Dart',
+//     description: 'Exercitation in consectetur velit enim ullamco aliqua officia mollit eu nulla sunt aliquip aute nulla. Aliquip tempor et exercitation minim adipisicing minim velit. Id elit culpa aute est. Aute aute excepteur et adipisicing adipisicing officia dolor. Commodo incididunt ad labore incididunt aliquip deserunt anim eu incididunt est mollit. Proident aliqua non aliquip dolore sint voluptate adipisicing. Tempor in fugiat incididunt ea officia ullamco ut qui excepteur dolore ut et ut. Ut do laborum culpa consectetur duis et velit est nulla. In consectetur nisi duis ad quis magna anim ut dolor minim irure proident do irure. Sit do quis sunt consequat id incididunt quis.',
+//     created_at: '23 hours ago'
+//   },
+//   {
+//     id: 5,
+//     tag: 'personal',
+//     title: 'Learn Dart',
+//     description: 'Exercitation in consectetur velit enim ullamco aliqua officia mollit eu nulla sunt aliquip aute nulla. Aliquip tempor et exercitation minim adipisicing minim velit. Id elit culpa aute est. Aute aute excepteur et adipisicing adipisicing officia dolor. Commodo incididunt ad labore incididunt aliquip deserunt anim eu incididunt est mollit. Proident aliqua non aliquip dolore sint voluptate adipisicing. Tempor in fugiat incididunt ea officia ullamco ut qui excepteur dolore ut et ut. Ut do laborum culpa consectetur duis et velit est nulla. In consectetur nisi duis ad quis magna anim ut dolor minim irure proident do irure. Sit do quis sunt consequat id incididunt quis.',
+//     created_at: '23 hours ago'
+//   },
+//   {
+//     id: 6,
+//     tag: 'personal',
+//     title: 'Learn Dart',
+//     description: 'Exercitation in consectetur velit enim ullamco aliqua officia mollit eu nulla sunt aliquip aute nulla. Aliquip tempor et exercitation minim adipisicing minim velit. Id elit culpa aute est. Aute aute excepteur et adipisicing adipisicing officia dolor. Commodo incididunt ad labore incididunt aliquip deserunt anim eu incididunt est mollit. Proident aliqua non aliquip dolore sint voluptate adipisicing. Tempor in fugiat incididunt ea officia ullamco ut qui excepteur dolore ut et ut. Ut do laborum culpa consectetur duis et velit est nulla. In consectetur nisi duis ad quis magna anim ut dolor minim irure proident do irure. Sit do quis sunt consequat id incididunt quis.',
+//     created_at: '23 hours ago'
+//   },
+// ];
